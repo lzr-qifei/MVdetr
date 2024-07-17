7bdd5dc700a26d6c1c32253ce7683f625f8bb560
root <1102733562@qq.com>
2024-07-17 16:48:14 +0800

    1.r50fixed 2.config

--- multiview_detector/runv100.sh
+++ multiview_detector/runv100.sh
@@ -1,5 +1,6 @@
 srun -p c7e6fad6-4dfa-42ef-af06-b0858c594d44 --workspace-id c75ef8a9-625f-4985-8c14-67b04e72a8c1 \
  -N 1 -f pt -d StandAlone -r N1lS.Ia.I20.1  -j mvdetr_r50_train  \
 bash -c 'export OMP_NUM_THREADS=1  && source activate /home/mnt/lizirui/envs/TrackTacular \
- && python /home/mnt/lizirui/MVDeTr-main/main.py  -d multiviewx --data /home/mnt/lizirui/data/MultiviewX --world_reduce 4 --arch resnet50 --pth /home/mnt/lizirui/MVDeTr-main/multiview_detector/r50.pth'
+ && python /home/mnt/lizirui/MVDeTr-main/main.py  -d multiviewx --data /home/mnt/lizirui/data/MultiviewX --world_reduce 4 --arch resnet50 \
+ --pth /home/mnt/lizirui/MVDeTr-main/multiview_detector/r50.pth --out_path /home/mnt/lizirui/MVDeTr-main/multiview_detector/results/test.txt'
 
